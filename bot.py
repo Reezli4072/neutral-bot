566f4789445cfa7a548e9dc86bec66e7309e5793
Reezli <106180526+Reezli4072@users.noreply.github.com>
2022-07-26 17:16:34 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,54 @@
+from cgitb import text
+
+from time import sleep
+from turtle import color, title
+from unittest import loader
+import discord
+import time
+
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix='.')
+
+@bot.event
+async def on_ready()
+    activity = discord.Game(name=neutral.tech, type=3)
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    print(Bot is ready!)
+
+@bot.command()
+@commands.has_role(CUSTOMER)
+async def download(ctx)
+    embed = discord.Embed(title=Generating Loader..., description = loader has been sended!,  color=0x9daff2)
+   
+    embed.set_thumbnail(url=httpscdn.discordapp.comattachments979715481160274007986384620398444634kutas.png)
+    
+    embed.set_footer(text=Check DM!)
+
+    await ctx.send(embed=embed)
+
+    time.sleep(2)
+
+    embed2 = discord.Embed(title=Loader,  color=0x9daff2)
+    embed2.set_thumbnail(url=httpscdn.discordapp.comattachments979715481160274007986384620398444634kutas.png)
+    embed2.add_field(name=u200b, value=)
+    
+    
+   
+   
+    
+    embed2.set_footer(text=neutral.tech)
+
+    await ctx.author.send(embed=embed2)
+    
+
+
+
+    
+
+
+
+
+
+bot.run()
